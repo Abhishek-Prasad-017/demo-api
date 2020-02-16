fa2ad39d053f610d2f3dcc00228700e4829b32d6
Abhishek Prasad <abhishpr@cisco.com>
2020-02-16 17:40:58 +0530

    Updating server files

---
+++ server/server.go
@@ -0,0 +1,37 @@
+package server
+
+import (
+	"github.com/buaazp/fasthttprouter"
+	"github.com/demo-api/service"
+	"github.com/spf13/viper"
+	"github.com/valyala/fasthttp"
+	"log"
+)
+
+//Server struct
+type Server struct {
+	router       *fasthttprouter.Router
+	mongoService *service.MongoService
+
+}
+
+
+func StartServer(path string, viper *viper.Viper) error {
+	//Create mongodb service
+	mongo, err := service.LoadMongo(viper)
+	if err != nil {
+		log.Fatalf("Could not connect to Mongo %s", err)
+	}
+
+	s := &Server{
+		router:       fasthttprouter.New(),
+		mongoService: mongo,
+	}
+
+	//Generate API paths
+	s.setupRoutes()
+
+	//Start the server for listening
+	return fasthttp.ListenAndServe(path, s.router.Handler)
+
+}
