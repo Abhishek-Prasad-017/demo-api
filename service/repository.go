3de10b92f5faaf12295b9e4cea519baf3a68d7e3
Abhishek Prasad <abhishpr@cisco.com>
2020-02-26 14:41:23 +0530

    Updating repository to catch other details from the backend

--- service/repository.go
+++ service/repository.go
@@ -1,14 +1,24 @@
 package service
 
-const collection = "software"
+const collection1 = "software"
+const collection2 = "service-details"
 
 type Software struct {
-	SoftwareId    string 	     `bson:"softwareId" json:"softwareId"`
+	SoftwareId    string 	    `bson:"softwareId" json:"softwareId"`
 	SoftwareName  string        `bson:"softwareName" json:"softwareName"`
-	LnaguageUsed  string        `bson:"languageUsed" json:"languageUsed"`
-	CreateDate   string       `bson:"createDate" json:"createDate"`
-	Owner  string      `bson:"owner" json:"owner"`
+	LanguageUsed  string        `bson:"languageUsed" json:"languageUsed"`
+	CreateDate   string       	`bson:"createDate" json:"createDate"`
+	Owner  string      			`bson:"owner" json:"owner"`
+	AdminGroupName string       `bson:"adminGroupName" json:"adminGroupName"`
+	DeveloperGroupName string   `bson:"developerGroupName" json:"developerGroupName"`
+	QaGroupName string        	`bson:"qaGroupName" json:"qaGroupName"`
+	ReleaseGroupName string  	`bson:"releaseGroupName" json:"releaseGroupName"`
+	AsgName    string 	    `bson:"asgName" json:"asgName"`
+	ServiceName    string 	    `bson:"serviceName" json:"serviceName"`
+	Designation    []string 	    `bson:"designation" json:"designation"`
+	People    []string 	    `bson:"people" json:"people"`
 }
 
 // Softwares is an array of Software structs
 type Softwares []Software
+
